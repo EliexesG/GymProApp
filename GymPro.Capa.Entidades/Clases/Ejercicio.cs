d74876ee7df1ea810d8f6cb92e3a4a45fea73d4c
ELIAS GABRIEL GONZALEZ LARA <egonzalezlar@est.utn.ac.cr>
2022-07-03 12:19:17 -0600

    Ejercicio Related

---
+++ GymPro.Capa.Entidades/Clases/Ejercicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPro.Capa.Entidades.Clases
+{
+    class Ejercicio
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public TipoEjercicio _TipoEjercicio { get; set; }
+        public byte[] Imagen { get; set; }
+        public byte[] Video { get; set; }
+    }
+}
---
+++ GymPro.Capa.Entidades/Clases/TipoEjercicio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPro.Capa.Entidades.Clases
+{
+    class TipoEjercicio
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
