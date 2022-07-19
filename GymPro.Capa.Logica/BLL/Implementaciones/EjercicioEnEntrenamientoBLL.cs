b96fe3d3eea7114f66900c6041600d7271ac17e3
ELIAS GABRIEL GONZALEZ LARA <egonzalezlar@est.utn.ac.cr>
2022-07-19 11:12:30 -0600

    19/7/2022 DAL Y BLL 80%

---
+++ GymPro.Capa.Logica/BLL/Implementaciones/EjercicioEnEntrenamientoBLL.cs
@@ -0,0 +1,166 @@
+﻿using GymPro.Capa.Datos.Implementaciones;
+using GymPro.Capa.Datos.Interfaces;
+using GymPro.Capa.Entidades.Implementaciones;
+using GymPro.Capa.Logica.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPro.Capa.Logica.BLL.Implementaciones
+{
+    public class EjercicioEnEntrenamientoBLL : IEjercicioEnEntrenamientoBLL
+    {
+        private IEjercicioEnEntrenamientoDAL oEjercicioEnEntrenamientoDAL;
+
+        public EjercicioEnEntrenamientoBLL()
+        {
+            oEjercicioEnEntrenamientoDAL = EjercicioEnEntrenamientoDAL.GetInstance();
+        }
+
+        #region Acceso a datos
+        /// <summary>
+        /// Elimina un Ejercicio de Entrenamiento de la base de datos
+        /// </summary>
+        /// <param name="pCodigoEntrenamiento">Codigo del entrenamiento relacionado al ejercicio a eliminar</param>
+        /// <param name="pCodigoEjercicio">Codigo del ejercicio de Entrenamiento a eliminar</param>
+        public void EliminarEjercicioEnEntrenamiento(int pCodigoEntrenamiento, int pCodigoEjercicio)
+        {
+            try
+            {
+
+                oEjercicioEnEntrenamientoDAL.EliminarEjercicioEnEntrenamiento(pCodigoEntrenamiento, pCodigoEjercicio);
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+
+        /// <summary>
+        /// Insertar un Ejercicio en Entrenamiento en la base de datos
+        /// </summary>
+        /// <param name="pEjercicioEnEntrenamiento">Ejercicio en Entrenamiento a eliminar</param>
+        public void InsertarEjercicioEnEntrenamiento(EjercicioEnEntrenamiento pEjercicioEnEntrenamiento)
+        {
+
+            try
+            {
+
+                oEjercicioEnEntrenamientoDAL.InsertarEjercicioEnEntrenamiento(pEjercicioEnEntrenamiento);
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+
+        /// <summary>
+        /// Modificar un Ejercicio en Entrenamiento en la base de datos
+        /// </summary>
+        /// <param name="pEjercicioEnEntrenamiento">Ejercicio en Entrenamiento a modificar</param>
+        public void ModificarEjercicioEnEntrenamiento(EjercicioEnEntrenamiento pEjercicioEnEntrenamiento)
+        {
+
+            try
+            {
+
+                oEjercicioEnEntrenamientoDAL.ModificarEjercicioEnEntrenamiento(pEjercicioEnEntrenamiento);
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene una lista de Ejercicios en Entrenamiento relacionados al Codigo de Entrenamiento de la base de datos
+        /// </summary>
+        /// <param name="pCodigoEntrenamiento">Codigo de Entrenamiento relacionado a los Ejercicios en Entrenamiento a buscar</param>
+        /// <returns>Lista de entidades de tipo EjericicioEnEntrenamiento</returns>
+        public List<EjercicioEnEntrenamiento> ObtenerEjercicioEnEntrenamientoCodigoEntrenamiento(int pCodigoEntrenamiento)
+        {
+
+            try
+            {
+
+                return oEjercicioEnEntrenamientoDAL.ObtenerEjercicioEnEntrenamientoCodigoEntrenamiento(pCodigoEntrenamiento);
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+
+        /// <summary>
+        /// Obtiene un Ejercicio en Entrenamiento relacionado a sus Ids de la base de datos
+        /// </summary>
+        /// <param name="pCodigoEntrenamiento"> Codigo del entrenamiento relacionado al ejercicio a buscar</param>
+        /// <param name="pCodigoEjercicio">Codigo del ejercicio de Entrenamiento a buscar</param>
+        /// <returns>Entidad de tipo EjercicioEnEntrenamiento</returns>
+        public EjercicioEnEntrenamiento ObtenerEjercicioEnEntrenamientoId(int pCodigoEntrenamiento, int pCodigoEjercicio)
+        {
+
+            try
+            {
+
+                return oEjercicioEnEntrenamientoDAL.ObtenerEjercicioEnEntrenamientoId(pCodigoEntrenamiento, pCodigoEjercicio);
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+
+        /// <summary>
+        /// Retorna una lista de todos los Ejercicios en Entrenamiento en la base de datos
+        /// </summary>
+        /// <returns>Lista de entidades de tipo EjercicioEnEntrenamiento</returns>
+        public List<EjercicioEnEntrenamiento> ObtenerEjercicioEnEntrenamientoTodos()
+        {
+
+            try
+            {
+
+                return oEjercicioEnEntrenamientoDAL.ObtenerEjercicioEnEntrenamientoTodos();
+
+            }
+            catch (SqlException sqlError)
+            {
+                throw sqlError;
+            }
+            catch (Exception er)
+            {
+                throw er;
+            }
+        }
+        #endregion
+    }
+}
---
+++ GymPro.Capa.Logica/BLL/Interfaces/IEjercicioEnEntrenamientoBLL.cs
@@ -0,0 +1,15 @@
+﻿using GymPro.Capa.Entidades.Implementaciones;
+using System.Collections.Generic;
+
+namespace GymPro.Capa.Logica.BLL.Interfaces
+{
+    public interface IEjercicioEnEntrenamientoBLL
+    {
+        void EliminarEjercicioEnEntrenamiento(int pCodigoEntrenamiento, int pCodigoEjercicio);
+        void InsertarEjercicioEnEntrenamiento(EjercicioEnEntrenamiento pEjercicioEnEntrenamiento);
+        void ModificarEjercicioEnEntrenamiento(EjercicioEnEntrenamiento pEjercicioEnEntrenamiento);
+        List<EjercicioEnEntrenamiento> ObtenerEjercicioEnEntrenamientoCodigoEntrenamiento(int pCodigoEntrenamiento);
+        EjercicioEnEntrenamiento ObtenerEjercicioEnEntrenamientoId(int pCodigoEntrenamiento, int pCodigoEjercicio);
+        List<EjercicioEnEntrenamiento> ObtenerEjercicioEnEntrenamientoTodos();
+    }
+}
